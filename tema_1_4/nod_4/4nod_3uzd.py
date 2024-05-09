46877e41681f24dc26bbfd97193baa40066ae222
Agris Ginters <Agris.Ginters@csp.gov.lv>
2024-05-09 19:24:55 +0300

    teksta rindas

--- tema_1_4/nod_4/4nod_3uzd.py
+++ tema_1_4/nod_4/4nod_3uzd.py
@@ -5,9 +5,11 @@ pirm_sk=True
 
 sk=int(input("ievadiet skaitli: "))
 
-while i<= int(math.sqrt(sk)):
+# while i<int(sk**0.5) # šādi var izmantot bez math
+while i<= int(math.sqrt(sk)): # šādi dara math bibliotēku
     if sk%i == 0:
         pirm_sk=False
+        break
 
     i+=1
 
@@ -15,3 +17,4 @@ if pirm_sk==True:
     print(f"Ievadītais skaitlis {sk} ir pirmskaitlis!")
 else:
     print(f"Ievadītais skaitlis {sk} NAV pirmskaitlis!")
+
---
+++ tema_1_4/nod_5/strings.py
@@ -0,0 +1,36 @@
+
+food = "pica"
+print(food)
+
+drink="kaffija"
+print(f"man garši {drink}")
+
+ttt="kaut kāds 'teksts'"
+print(ttt)
+
+teksts='es saku, \'gribu ēst\''
+print(teksts)
+
+teksts="divas \nrindas"
+print(teksts)
+
+gara_rinda="""kaut kas
+vēl kaut kaut kas
+    arī vēl kaut kas"""
+
+print(gara_rinda)
+
+name="Agris"
+age=42
+f_str=f"mani sauc {name} un man ir {age} gadi"
+print(f_str)
+
+r=10
+PI=3.1415926
+
+area=PI * r ** 2
+area_string = f"radiuss {r} ir {area:.2f}"
+print(area_string)
+
+area_string = f"radiuss {r:05} ir {area:.3f}"
+print(area_string)
