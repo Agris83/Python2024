7c7eb3b37056ec6f50a9e659eaa3a747eb56987b
Agris Ginters <Agris.Ginters@csp.gov.lv>
2024-05-16 17:43:58 +0300

    6. nodaļas 3. uzdevus

--- tema_1_4/nod_6/nod6_uzd3.py
+++ tema_1_4/nod_6/nod6_uzd3.py
@@ -3,10 +3,18 @@ rev_sar=[]
 
 teksts=input("Ievadiet tekstu: ")
 
-saraksts=teksts.split()
+saraksts=teksts.lower().split()
 
 for el in saraksts:
     rev_sar.append(el[::-1])
 
+teksts=""
+for el in rev_sar:
+    if len(teksts)>0:
+        teksts+=" "        
+    teksts+=el
+    
+
 print(rev_sar)
+print(teksts.capitalize())
 
