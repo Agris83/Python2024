0981fedd5b4112d4c32f35bb545984e03a3f768e
Agris Ginters <Agris.Ginters@csp.gov.lv>
2024-05-16 18:07:04 +0300

    nod6 4. uzd

---
+++ tema_1_4/nod_6/nod6_uzd4.py
@@ -0,0 +1,23 @@
+
+sk=2
+i=2
+skaitli=[]
+cik=int(input("Ievadiet pirmskaitļu skaitu: "))
+
+
+while len(skaitli)<cik+1:
+    pirm_sk=True
+    i=2
+
+    while i<=int(sk**0.5):
+        if sk%i == 0:
+            pirm_sk=False
+            break
+        i+=1
+
+    if pirm_sk==True:
+        skaitli.append(sk)
+
+    sk+=1
+
+print(skaitli)
