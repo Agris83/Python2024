5a67ed9118af9206ee86541a4faf9cbccea43d23
Agris Ginters <Agris.Ginters@csp.gov.lv>
2024-05-20 19:50:19 +0300

    uzlabots 3 uzdevuma risinājums

--- tema_1_4/nod_7/nod7_uzd2.py
+++ tema_1_4/nod_7/nod7_uzd2.py
@@ -1,13 +1,17 @@
 
-def is_palindrome(text):
+# šādi definē tipus, bet Python tāpat strādās...
+
+def is_palindrome(text: str) -> bool:
     text=text.lower()
     text=text.replace(" ","")
     rev_text=text[::-1]
 
-    if rev_text==text:
-        return True
-    else:
-        return False
+    return rev_text==text # var rakstīt vnk - šādi
+
+    # if rev_text==text:
+    #     return True
+    # else:
+    #     return False
     
     
 ievads=input("Ievadiet kaut kādu tekstu: ")
--- tema_1_4/nod_7/nod7_uzd3.py
+++ tema_1_4/nod_7/nod7_uzd3.py
@@ -4,16 +4,21 @@ def get_city_year(p0, perc, delta, p_target):
     rez=p0
     
     while rez<=p_target and i>-1:
-        rez=rez+rez*perc*0.01+delta    
+        rez=int(rez+rez*perc*0.01+delta)
         i+=1
 
         if rez<=p0:
             i=-1
-
+             
+        p0=rez
     return i
 
 
-a=get_city_year(1500, 5, 100, 5000)
+# a=get_city_year(1000, 2, 50, 1200)
+# a=get_city_year(1000, 2, -50, 5000)
+# a=get_city_year(1500, 5, 100, 5000)
+# a=get_city_year(1500000, 2.5, 10000, 2000000)
+
+a=get_city_year(1000, -3, 40, 2000)
 
 print(a)
-
