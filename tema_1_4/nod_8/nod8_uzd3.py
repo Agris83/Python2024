9add34925c7a7f3f55409044f38d43b5138063ad
Agris Ginters <Agris.Ginters@csp.gov.lv>
2024-05-21 21:17:15 +0300

    3b

--- tema_1_4/nod_8/nod8_uzd3.py
+++ tema_1_4/nod_8/nod8_uzd3.py
@@ -10,6 +10,15 @@ def clean_dict_value(d, bad_val):
 
 print(clean_dict_value({'a':5,'b':6,'c':5}, 5))
 
+# 3a
+def clean_dict_values(d, bad_sar):
+    d_kopy=d.copy()
 
+    for key, val in d.items():
+        for dd in bad_sar:
+            if val==dd:
+                if key in d_kopy:
+                    del d_kopy[key]
+    return(d_kopy)
 
-
+print(clean_dict_values({'a':5,'b':6,'c':5}, [3,4,5]))
