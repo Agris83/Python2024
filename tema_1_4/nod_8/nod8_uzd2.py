8238354f31bd007edc2ee830f0cf88d2f53962b7
Agris Ginters <Agris.Ginters@csp.gov.lv>
2024-05-21 20:49:46 +0300

    8. nodaļas 2. uzdevums

---
+++ tema_1_4/nod_8/nod8_uzd2.py
@@ -0,0 +1,10 @@
+
+def replace_dict_value(d, bad_val, good_val):
+
+    for key, val in d.items():
+        if val==bad_val:
+            d[key]=good_val
+
+    print(d)
+
+replace_dict_value({'a':5,'b':6,'c':5}, 5, 10) 
